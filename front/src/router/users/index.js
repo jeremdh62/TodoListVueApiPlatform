4618e13891dff91d4bd6e19ea62ed05f1c0ea6d4
jeremdh62 <jdhaille@myges.fr>
2023-09-20 14:27:06 +0200

    update send email

--- front/src/router/users/index.js
+++ front/src/router/users/index.js
@@ -1,16 +1,32 @@
 const Users = () => import('@/pages/users/UserList.vue');
 const PdfEdit = () => import('@/pages/users/UserEdit.vue');
 
+import { store } from '@/store';
+
 const routeUsers = [
     {
         path: '/users',
         name: 'UsersList',
-        component: Users
+        component: Users,
+        beforeEnter: (to, from, next) => {
+            if (store.getters['authStore/getRoles'].includes('ROLE_ADMIN')) {
+                next();
+            } else {
+                next('/');
+            }
+        }
     },
     {
         path: '/users/:id',
         name: 'UserEdit',
-        component: PdfEdit
+        component: PdfEdit,
+        beforeEnter: (to, from, next) => {
+            if (store.getters['authStore/getRoles'].includes('ROLE_ADMIN')) {
+                next();
+            } else {
+                next('/');
+            }
+        }
     },
 ];
 
--- front/src/store/sidebar/index.js
+++ front/src/store/sidebar/index.js
@@ -10,14 +10,7 @@ const roles = jwtDecoded ? jwtDecoded.roles : null;
 const sidebarStore = {
     namespaced: true,
     state: () => ({
-        sidebar: [
-            {
-                id: uuidv4(),
-                icon: 'grid-alt',
-                title: 'Kanban',
-                url:   '/',
-            },
-        ]
+        sidebar: getSidebarWithRole()
     }),
     getters: {
         getSidebar: (state) => {
@@ -26,25 +19,40 @@ const sidebarStore = {
     }
 };
 
-if (roles && roles.includes('ROLE_ADMIN')) {
-    sidebarStore.state.sidebar.push({
-        id: uuidv4(),
-        icon: 'users',
-        title: 'User Management',
-        url:   '',
-        subMenus: [
-            {
-                id: uuidv4(),
-                title: 'Users List',
-                url: '/users'
-            },
-            {
-                id: uuidv4(),
-                title: 'User Add',
-                url: '/users/add'
-            },
-        ]
-    });
+
+function getSidebarWithRole() {
+
+    const sidebar = [
+        {
+            id: uuidv4(),
+            icon: 'grid-alt',
+            title: 'Kanban',
+            url:   '/',
+        },
+    ]
+
+    if (roles && roles.includes('ROLE_ADMIN')) {
+        sidebar.push({
+            id: uuidv4(),
+            icon: 'users',
+            title: 'User Management',
+            url:   '',
+            subMenus: [
+                {
+                    id: uuidv4(),
+                    title: 'Users List',
+                    url: '/users'
+                },
+                {
+                    id: uuidv4(),
+                    title: 'User Add',
+                    url: '/users/add'
+                },
+            ]
+        });
+    }
+
+    return sidebar;
 }
 
 
